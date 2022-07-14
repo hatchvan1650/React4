eefdbb46fe198798fe83849ae01f33b0f2cc3e46
Van <100538869+hatchvan1650@users.noreply.github.com>
2022-07-14 09:17:13 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <Search />
+    </div>
+  );
+}
+
+<a href="https://github.com/hatchvan1650/Vanilla-weatherapp.git"> Git </a>;
